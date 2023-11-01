f1f4c9d2daebbe6bf234047f229e59c2b7c4c32d
mikolajdevs <mikolajdevs@gmail.com>
2023-11-01 20:23:59 +0100

    chore: init javafx app

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,38 @@
 package org.example;
 
-public class App {
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    public static Parent loadFXML(String fileName) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fileName + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(loadFXML("menu"));
+        stage.setTitle("SnakeX");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        System.exit(0);
+        Platform.exit();
     }
 }
